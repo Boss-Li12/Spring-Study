dad1d66ce02afa502c85ca8060a41ac730c6332d
Irving <546966163@qq.com>
2022-10-11 14:28:17 +0800

    mybatis attr need to be same

--- spring-10-mybatis/src/main/java/com/pojo/User.java
+++ spring-10-mybatis/src/main/java/com/pojo/User.java
@@ -1,16 +1,17 @@
 package com.pojo;
 
 public class User {
+    // 名字必须对应！
     private int id;
     private String name;
-    private String pwd;
+    private String password;
 
     @Override
     public String toString() {
         return "User{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
-                ", pwd='" + pwd + '\'' +
+                ", pwd='" + password + '\'' +
                 '}';
     }
 }
